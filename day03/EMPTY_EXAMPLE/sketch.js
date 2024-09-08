3690ce4ae538b813b4ed7ef95f5d602054e3208a
craignyu <kapp@cs.nyu.edu>
2024-09-08 06:12:18 -0400

    Days 02/03

---
+++ day03/EMPTY_EXAMPLE/sketch.js
@@ -0,0 +1,9 @@
+// setup function - used for commands that need to run only once
+function setup() {
+  createCanvas(500,500);
+}
+
+// draw function - used for commands that need to be repeated
+function draw() {
+
+}
