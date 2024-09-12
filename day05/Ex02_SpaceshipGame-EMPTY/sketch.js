c74fa319224e8332f2a790456ad9d1b0ed6667ef
craignyu <kapp@cs.nyu.edu>
2024-09-12 08:26:08 -0400

    Days 04 / 05

---
+++ day05/Ex02_SpaceshipGame-EMPTY/sketch.js
@@ -0,0 +1,18 @@
+// load in our graphics
+let bg;
+let ufo;
+let cow;
+
+function preload() {
+  bg = loadImage('../images/background.jpg');
+  ufo = loadImage('../images/ufo.png');
+  cow = loadImage('../images/cow.png');
+}
+
+function setup() {
+  createCanvas(500,500);
+}
+
+function draw() {
+
+}
